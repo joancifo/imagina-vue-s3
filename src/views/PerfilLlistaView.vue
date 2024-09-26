340f01413e3e4b765deae117f22015cb64d82707
Joan Cifo <joancifo@gmail.com>
2024-09-26 12:09:26 +0200

    minor

--- src/views/PerfilLlistaView.vue
+++ src/views/PerfilLlistaView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const { query } = useRoute()
