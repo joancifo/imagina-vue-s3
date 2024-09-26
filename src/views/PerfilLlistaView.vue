2d777d1675a4519636588b8d3a0f029133598330
Joan Cifo <joancifo@gmail.com>
2024-09-26 11:03:16 +0200

    feat: fetch then catch

--- src/views/PerfilLlistaView.vue
+++ src/views/PerfilLlistaView.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const usuaris = ref<any[]>([])
+
+const carregarUsuaris = () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      usuaris.value = response
+    })
+    .catch((error) => {
+      console.log({ error })
+    })
+
+  // console.log(response)
+
+  // usuaris.value = resultat
+}
+
+carregarUsuaris()
+</script>
 <template>
   <div>
     <h1>Llista d'usuaris</h1>
-    <ul></ul>
+    <ul v-if="usuaris.length">
+      <li v-for="usuari in usuaris" :key="usuari.id">{{ usuari.name }} {{ usuari.phone }}</li>
+    </ul>
+    <div v-else>No hi ha cap usuari</div>
   </div>
 </template>
