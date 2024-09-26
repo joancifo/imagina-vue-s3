5f99471c973b117d288fe10372ff4c6ab482d4a4
Joan Cifo <joancifo@gmail.com>
2024-09-26 13:33:05 +0200

    feat: vue-router push

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,26 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const confirmar = () => {
+  // comprovacions
+  const validat = true
+
+  if (validat) {
+    router.push({ name: 'account' })
+  } else {
+    router.back()
+  }
+}
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <div>
+      <button @click="confirmar">Confirmar compra!</button>
+    </div>
   </div>
 </template>
 
