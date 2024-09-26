ff5b30cc6697a1c4a73a9a1c25a8282e41fac375
Joan Cifo <joancifo@gmail.com>
2024-09-26 10:13:27 +0200

    refactor: canviar rutes per name

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hola Vue 3" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+        <RouterLink :to="{ name: 'about' }">Sobre nosaltres</RouterLink>
       </nav>
     </div>
   </header>
