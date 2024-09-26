7c6c1002ea318929defe7374b3e1cdc047f00bb3
Joan Cifo <joancifo@gmail.com>
2024-09-26 13:01:34 +0200

    feat: router beforeEach

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,23 +21,42 @@ const router = createRouter({
       path: '/usuari',
       name: 'perfil-index',
       component: () => import('@/views/PerfilLlistaView.vue'),
-      beforeEnter: () => {
-        // const token = sessionStorage.getItem('access_token')
-        const token = 'adasdasdad.asdasdasd'
-
-        if (!token) {
-          return {
-            name: 'home'
-          }
-        }
+      meta: {
+        requireAuth: true
       }
+      // beforeEnter: () => {
+      //   // const token = sessionStorage.getItem('access_token')
+      //   const token = 'adasdasdad.asdasdasd'
+
+      //   if (!token) {
+      //     return {
+      //       name: 'home'
+      //     }
+      //   }
+      // }
     },
     {
       path: '/usuari/:id',
       name: 'perfil',
-      component: () => import('@/views/PerfilView.vue')
+      component: () => import('@/views/PerfilView.vue'),
+      meta: {
+        requireAuth: true
+      }
     }
   ]
+})
+
+router.beforeEach((to: any, from) => {
+  if (to.meta.requireAuth) {
+    const token = sessionStorage.getItem('access_token')
+    // const token = 'adasdasdad.asdasdasd'
+
+    if (!token) {
+      return {
+        name: 'home'
+      }
+    }
+  }
 })
 
 export default router
