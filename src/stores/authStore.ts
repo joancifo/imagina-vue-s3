1604d7882ece76359f7689e3785b0621992c3b97
Joan Cifo <joancifo@gmail.com>
2024-09-30 12:41:56 +0200

    feat: pinia-persist

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -2,25 +2,32 @@ import { defineStore } from 'pinia'
 import { ref, type Ref } from 'vue'
 import httpClient from '@/plugins/httpClient'
 
-export const useAuthStore = defineStore('auth', () => {
-  const authToken: Ref<string> = ref<string>(sessionStorage.getItem('access_token') || '')
+export const useAuthStore = defineStore(
+  'auth',
+  () => {
+    const authToken: Ref<string> = ref<string>('')
 
-  const login = async (usuari: string, contrasenya: string) => {
-    try {
-      const { data }: any = await httpClient.post('/users', {
-        usuari,
-        contrasenya
-      })
-      //   authToken.value = response.auth_token
-      authToken.value = data.usuari // fake token
-      sessionStorage.setItem('access_token', authToken.value || '')
-    } catch (error) {
-      console.log({ error }, 'Error amb login!')
+    const login = async (usuari: string, contrasenya: string) => {
+      try {
+        const { data }: any = await httpClient.post('/users', {
+          usuari,
+          contrasenya
+        })
+        //   authToken.value = response.auth_token
+        authToken.value = data.usuari // fake token
+      } catch (error) {
+        console.log({ error }, 'Error amb login!')
+      }
+    }
+
+    return {
+      authToken,
+      login
+    }
+  },
+  {
+    persist: {
+      storage: sessionStorage
     }
   }
-
-  return {
-    authToken,
-    login
-  }
-})
+)
